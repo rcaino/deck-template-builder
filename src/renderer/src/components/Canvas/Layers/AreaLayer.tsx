c9510cd39e1089fa90df4f3b488e9f5a6fb3e6ad
RoyJr <rogrjt@gmail.com>
2026-08-12 23:24:40 -0300

    fix: solucionar tipado de border en AreaLayer

--- src/renderer/src/components/Canvas/Layers/AreaLayer.tsx
+++ src/renderer/src/components/Canvas/Layers/AreaLayer.tsx
@@ -7,28 +7,28 @@ interface AreaLayerProps extends ILayerProps, IAreaLayer {
   children?: React.ReactNode;
 }
 
-const getPath = (path: AreaLayerProps["path"]): React.CSSProperties["clipPath"] => {
+const getPath: (path: AreaLayerProps["path"]) => React.CSSProperties["clipPath"] = (path) => {
   switch (path) {
     case "circle":
-      return "circle(50% at 50% 50%)"; // Sin punto y coma
+      return "circle(50% at 50% 50%);";
     case "hexagon":
-      return "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)";
+      return "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);";
     case "l-chevron":
-      return "polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)";
+      return "polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%);";
     case "l-point":
-      return "polygon(25% 0%, 100% 1%, 100% 100%, 25% 100%, 0% 50%)";
+      return "polygon(25% 0%, 100% 1%, 100% 100%, 25% 100%, 0% 50%);";
     case "pentagon":
-      return "polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)";
+      return "polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%);";
     case "r-chevron":
-      return "polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%)";
+      return "polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%);";
     case "r-point":
-      return "polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)";
+      return "polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%);";
     case "rhombus":
-      return "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)";
+      return "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);";
     case "star":
-      return "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 80% 100%, 50% 70%, 20% 100%, 32% 57%, 2% 35%, 39% 35%)";
+      return "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 80% 100%, 50% 70%, 20% 100%, 32% 57%, 2% 35%, 39% 35%);";
     case "triangle":
-      return "polygon(50% 0%, 0% 100%, 100% 100%)";
+      return "polygon(0% 0%, 0% 100%, 100% 100%);";
     default:
       return path;
   }
